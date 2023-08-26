51e0b2693ccf193218c24dbc960cd2dc1274f9b1
Geert Vanderkelen <geert@vanderkelen.org>
2023-08-26 16:14:26 +0200

    refactor: move DSN functions to github.com/golistic/xgo/xsql
    We keep the original functions as deprecated.

--- dsn.go
+++ dsn.go
@@ -1,33 +1,16 @@
-// Copyright (c) 2022, Geert JM Vanderkelen
+// Copyright (c) 2023, Geert JM Vanderkelen
 
 package xmysql
 
 import (
-	"regexp"
-	"strings"
-
-	"github.com/go-sql-driver/mysql"
+	"github.com/golistic/xgo/xsql"
 )
-
-var reDSNPassword = regexp.MustCompile(`^(.*):[^/]*?(@.*)$`)
-var dsnPasswordMask = "********"
 
 // ReplaceDSNDatabase takes dsn and replaces the database name. It
 // returns the new Data Source Name.
+// Deprecated: use github.com/golistic/xgo/xsql
 func ReplaceDSNDatabase(dsn string, name string) (string, error) {
-	config, err := mysql.ParseDSN(dsn)
-	if err != nil {
-		return "", NewError(err)
-	}
-
-	config.DBName = name
-
-	newDSN := config.FormatDSN()
-	if _, err := mysql.ParseDSN(dsn); err != nil {
-		return "", NewError(err)
-	}
-
-	return newDSN, nil
+	return xsql.ReplaceDSNDatabase(dsn, name)
 }
 
 // SetDSNParams sets parameters for the given dsn.
@@ -35,25 +18,9 @@ func ReplaceDSNDatabase(dsn string, name string) (string, error) {
 // Some parameters are always set, if not provided through parameters or
 // when none were given:
 // * parseTime = true
+// Deprecated: use github.com/golistic/xgo/xsql
 func SetDSNParams(dsn string, parameters map[string]string) (string, error) {
-	config, err := mysql.ParseDSN(dsn)
-	if err != nil {
-		return "", NewError(err)
-	}
-
-	if config.Params == nil {
-		config.Params = map[string]string{}
-	}
-
-	for k, v := range parameters {
-		config.Params[k] = v
-	}
-
-	if _, ok := config.Params["parseTime"]; !ok {
-		config.Params["parseTime"] = "true"
-	}
-
-	return config.FormatDSN(), nil
+	return xsql.SetDSNOptions(dsn, parameters)
 }
 
 // MaskPasswordInDSN masks the password within the MySQL data source name dsn. This
@@ -62,22 +29,7 @@ func SetDSNParams(dsn string, parameters map[string]string) (string, error) {
 // When password is empty (not provided) the mask is added anyway.
 // When the DSN is something that was not a DSN, the mask itself is returned to
 // prevent possible mistakes.
+// Deprecated: use github.com/golistic/xgo/xsql
 func MaskPasswordInDSN(dsn string) string {
-	var res = dsn
-
-	if reDSNPassword.MatchString(dsn) {
-		res = reDSNPassword.ReplaceAllString(dsn, `$1:`+dsnPasswordMask+`$2`)
-	} else {
-		if strings.Contains(dsn, ":@") {
-			strings.Replace(dsn, ":@", ":"+dsnPasswordMask+"@", 1)
-		} else {
-			strings.Replace(dsn, "@", ":"+dsnPasswordMask+"@", 1)
-		}
-	}
-
-	if res == dsn {
-		return dsnPasswordMask
-	}
-
-	return res
+	return xsql.MaskPasswordInDSN(dsn)
 }
